37b83503e295767bb76f977567091dcc5832281a
mgomez05 <gomezruss7@gmail.com>
2022-09-08 18:13:24 -0400

    Added the exercises to the file exercises.js

---
+++ exercises.js
@@ -0,0 +1,14 @@
+// ms: number of milliseconds
+// returns a Promise that is resolved after ms milliseconds
+function delay(ms) {
+}
+
+// el: element node object
+// moves the element to the right by 100px over a duration of 1 second
+function animateRight(el) {
+}
+
+// xs: array
+// returns: a new array, with unique items
+function removeDuplicates(xs) {
+}
